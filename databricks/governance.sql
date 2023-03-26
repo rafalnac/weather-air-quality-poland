7147b63bf71003d739322adabed363dd7fca7237
rafal.naczelnik@gmail.com <rafal.naczelnik@gmail.com>
2023-03-26 14:57:48 +0000

    Add sql notebook to create lakehouse objects for governance.
    Notebook to store commads to create Catalogs, Schemas and other lakehouse objects.
    Note: Tables and views are created directly in each notebook.

---
+++ databricks/governance.sql
@@ -0,0 +1,11 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC ##### SQL notebook for creation objects like catalogs, schemas etc.
+
+-- COMMAND ----------
+
+CREATE CATALOG IF NOT EXISTS project_weather_air;
+
+-- COMMAND ----------
+
+CREATE SCHEMA IF NOT EXISTS project_weather_air.air_quality;
