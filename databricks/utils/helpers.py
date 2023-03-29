3e780c032ce78555f8052e51d56db54297b6eb63
rafal.naczelnik@gmail.com <rafal.naczelnik@gmail.com>
2023-03-29 17:17:50 +0000

    Added new function named 'deduplicate_df' to helpers.py

--- databricks/utils/helpers.py
+++ databricks/utils/helpers.py
@@ -1,7 +1,7 @@
 """Module provided functions to simplify data transfromations inside notebooks."""
 
 from typing import Union
-from pyspark.sql.functions import regexp_extract, to_timestamp, Column
+from pyspark.sql.functions import regexp_extract, to_timestamp, Column, DataFrame
 
 
 def parse_file_timestamp(
@@ -41,3 +41,35 @@ def extract_timestamp_string(
     """
 
     return regexp_extract(text, pat, 0)
+
+
+def deduplicate_df(
+    df: DataFrame, timestamp_col: str, partition_by: str
+) -> DataFrame:
+    """
+    Deduplicate dataframe based on maximum value of timestamp_col
+    for specified partition_by group.
+
+    Function deduplicates provided dataframe using window funcion.
+    It uses aggregate funcion MAX() to get latest timestamp(timestamp_col)
+    for every group(partition_by).
+
+    Args:
+        timestamp_col: Name of the column from dataframe based on what window funicion MAX()
+            will be executed.
+        partition_by: Name of the column from dataframe to define grouping scope for MAX() function.
+
+    Returns: Deduped dataframe with addtional column 'max_timestamp'
+
+    Raises:
+        TypeError Exception: When column is not a string type.
+    """
+
+    if not isinstance(timestamp_col, str) or not isinstance(partition_by, str):
+        raise TypeError("Column must be a string")
+
+    df_dedup = df.selectExpr(
+        "*", f"MAX({timestamp_col}) OVER (PARTITION BY {partition_by}) AS max_timestamp"
+    ).where(f"{timestamp_col} = max_timestamp")
+
+    return df_dedup
