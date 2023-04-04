2e0e32cdd0f893b618763b14214315a5b206e968
Rafal Naczelnik <rafal.naczelnik@gmail.com>
2023-04-04 10:32:55 +0200

    Moved serialize_to_iso funciton to helpers.

--- az-functions/adf-http-trigger/__init__.py
+++ az-functions/adf-http-trigger/__init__.py
@@ -7,12 +7,5 @@ import pyodbc
 import azure.functions as func
 
 
-def serialize_to_iso(obj: datetime.datetime | datetime.date) -> str:
-    """Serialize object to iso format string"""
-
-    if isinstance(obj, (datetime.datetime, datetime.date)):
-        return obj.isoformat()
-
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
