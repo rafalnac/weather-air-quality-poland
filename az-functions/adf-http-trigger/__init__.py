620294f2bd964bd442f882d75527284c49b9a4f6
Rafal Naczelnik <rafal.naczelnik@gmail.com>
2023-04-11 00:12:35 +0200

    Added code to convert received data from API to JSON file and save it in the blob container.

--- az-functions/adf-http-trigger/__init__.py
+++ az-functions/adf-http-trigger/__init__.py
@@ -1,10 +1,13 @@
 import logging
 import json
-import requests
+import datetime
 
+import requests
 import pyodbc
 import azure.functions as func
-from .connect_config import syn_sql_pool_conn_string
+
+from .connection_config import syn_sql_pool_conn_string, container_weather_dir_raw_data
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
@@ -15,8 +18,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Established connection to server")
 
     # Define query to select measure point ids from view [air_quality].[v_measure_points]
-    query_select = """
-    SELECT TOP 3
+    query_select = f"""
+    SELECT
         [point_id]
     FROM [air_quality].[v_measure_points]
     """
@@ -36,13 +39,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # Unpack nested tuples to list
     records_flat = [item for nested_tuple in records for item in nested_tuple]
 
+    # To do:
+    # 1 function to flat the records
+    # 2 function to iterate through the list and call measures data
+
     # Initialize a list to store JSON objects
     api_list = []
 
-    # Loop through each point_id 
+    # Loop through each point_id
     for point_id in records_flat:
-    
         # Get measurment data for each point_id
+        # To do:
+        # catch exceptions and act accordingly
         reqs = requests.get(
             url=f"https://api.gios.gov.pl/pjp-api/rest/data/getData/{point_id}"
         )
@@ -52,17 +60,25 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         # Specify point_id key to update JSON
         point_id_to_insert = {"point_id": point_id}
 
-        # Update_json with point id
+        # Update JSON with point id
         req_json.update(point_id_to_insert)
 
-        # Append to the list
+        # Append JSONs to the list
         api_list.append(req_json)
-    
-    # Convert list to JSON
+
+    # Convert list with nested JSONs to string
     api_json = json.dumps(obj=api_list)
 
-    return func.HttpResponse(
-        body=api_json,
-        status_code=200
+    # Current utc timestamp
+    current_timestamp_utc = datetime.datetime.isoformat(
+        datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
     )
 
+    # Create blob in container
+    container_weather_dir_raw_data.upload_blob(
+        name=f"measure_points_data_{current_timestamp_utc}.json",
+        data=api_json,
+        blob_type="BlockBlob",
+    )
+
+    return func.HttpResponse(status_code=200)
