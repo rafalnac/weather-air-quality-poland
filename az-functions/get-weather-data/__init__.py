016a65ddf32799c484bbdd5ed325a678d0d25ca5
Rafal Naczelnik <rafal.naczelnik@gmail.com>
2023-04-18 12:46:16 +0200

    Added logic to function main(), to ingest data from weather API and return it as json formatted string as HttpResponse.

--- az-functions/get-weather-data/__init__.py
+++ az-functions/get-weather-data/__init__.py
@@ -1,24 +1,37 @@
+import datetime
+import json
 import logging
+import requests
+import time
 
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request.")
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    # API URL
+    weather_data_api_url = "https://danepubliczne.imgw.pl/api/data/synop"
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+    # Explicitly set encoding to corectlly read polish characters
+    headers = {"charset": "utf-8 BOM"}
+    # API call
+    try:
+        reqst = requests.get(weather_data_api_url, headers=headers)
+    # Rerun the request in case of timeout
+    except requests.exceptions.Timeout as e:
+        num_sec_to_wait = 5
+        logging.info(
+            f"{e}.\nTrying again to perform API request in {num_sec_to_wait} sec."
         )
+        time.sleep(num_sec_to_wait)
+        reqst = requests.get(weather_data_api_url, headers=headers)
+
+    # Convert Response object to JSON
+    reqst_data = reqst.json()
+
+    # Convert JSON to string.
+    # Param ensure_ascii is set to false because JSON contains polish chars
+    json_str = json.dumps(reqst_data, ensure_ascii=False)
+
+    return func.HttpResponse(json_str, status_code=200)
