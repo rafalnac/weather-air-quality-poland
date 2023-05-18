c7891e315db72cf1905fd747212023a503c73a8a
rafal.naczelnik@gmail.com <rafal.naczelnik@gmail.com>
2023-05-18 11:57:00 +0000

    Denormalized tables: Joined Params to Points table.

--- databricks/air_quality/gios_measure_points.py
+++ databricks/air_quality/gios_measure_points.py
@@ -14,8 +14,8 @@ from databricks.utils.helpers import extract_timestamp_string, parse_file_timest
 # MAGIC #### Ingest sensor data (BRONZE)
 # MAGIC Each station has muliple sensors. Each sensor measures level of different chemical compound.
 # MAGIC - Each file comes in the json format
-# MAGIC - Expect that each file name contains timestamp in format 'yyyy-MM-dd'T'HH_mm_ss_SSSSSSSX'.
-# MAGIC - File name example 'measure_point_station_10005_2023-03-27T20_25_43_6291508Z.json'
+# MAGIC - Expect that each file name contains timestamp.
+# MAGIC - File name example 'measure_points_station_2023-05-18T07_41_15.611049+00_00.json'
 # MAGIC - Timestamp pattern format according to: https://spark.apache.org/docs/latest/sql-ref-datetime-pattern.html
 
 # COMMAND ----------
@@ -53,8 +53,9 @@ measure_points_schema = StructType(
 
 measure_points_df = (
     spark.readStream.format("json")
+    .option("mulitLine", "true")
     .schema(measure_points_schema)
-    .load(os.path.join(RAW_DATA_PATH, "measure_points"))
+    .load(os.path.join(RAW_DATA_PATH, "measure_points/*.json"))
 )
 
 # COMMAND ----------
@@ -73,7 +74,10 @@ measure_points_audit_col = (
     .withColumn("source_file", input_file_name())
     .withColumn(
         "file_timestamp",
-        parse_file_timestamp(extract_timestamp_string(col("source_file"))),
+        parse_file_timestamp(
+            timestamp_str=extract_timestamp_string(col("source_file"), pat="\d{4}-\d{2}-\d{2}T\d{2}_\d{2}_\d{2}"),
+            parse_pat="yyyy-MM-dd'T'HH_mm_ss"
+        )
     )
 )
 
@@ -119,12 +123,11 @@ LOCATION "{MEASURE_POINTS_BRONZE_SAVE_LOC}"
 # MAGIC 3) Create silver talbe (if not already exist)
 # MAGIC 4) Upsert:
 # MAGIC       - Deduplicate microbatch based on the latest file timestamp
-# MAGIC       - Upsert to silver table as SCD Type 1
+# MAGIC       - Upsert to silver table
 
 # COMMAND ----------
 
 MEASURE_POINTS_SILVER_LOC = os.path.join(SILVER_STAGE_DIR_PATH, 'measure_points_silver')
-PARAMS_SILVER_LOC = os.path.join(SILVER_STAGE_DIR_PATH, 'params_silver')
 
 # COMMAND ----------
 
@@ -146,55 +149,32 @@ monitor_stations_bronze_df = spark.readStream.table(
 
 monitor_stations_expand_rename_cols = (
     monitor_stations_bronze_df
-        .select("id", "param.idParam", "param.paramCode", "param.paramName", "stationId", "file_timestamp")
+        .select("id", "param.idParam", "param.paramCode", "stationId", "file_timestamp")
         .withColumnRenamed("id", "point_id")
-        .withColumnRenamed("idParam", "param_id")
         .withColumnRenamed("stationId", "station_id")
-        .withColumnRenamed("paramName", "param_name")
+        .withColumnRenamed("idParam", "param_id")
         .withColumnRenamed("paramCode", "param_code")
 )
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ##### Create measure points silver table (Dimension SCD Type 1)
+# MAGIC ##### Create measure points silver table
 
 # COMMAND ----------
 
 spark.sql(f"""
-CREATE TABLE IF NOT EXISTS project_weather_air.air_quality.measure_points_silver
+CREATE TABLE IF NOT EXISTS project_weather_air.air_quality.measure_points_silver 
   (
-    point_sk BIGINT GENERATED ALWAYS AS IDENTITY (START WITH 1),
     point_id INT,
     station_id INT,
     param_id INT,
-    proccessed_timestamp TIMESTAMP
+    param_code STRING,
+    processed_timestamp TIMESTAMP
   )
 USING DELTA
-COMMENT "Measure points stations."
+COMMENT "Measure points with their parameters"
 LOCATION "{MEASURE_POINTS_SILVER_LOC}"
-""")
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ##### Create parameter silver table (Dimension SCD Type 1)
-# MAGIC - Table with parameters codes and names
-
-# COMMAND ----------
-
-spark.sql(f"""
-CREATE TABLE IF NOT EXISTS project_weather_air.air_quality.params_silver
-(
-  param_sk BIGINT GENERATED ALWAYS AS IDENTITY (START WITH 1),
-  param_id INT,
-  param_code STRING,
-  param_name STRING,
-  processed_timestamp TIMESTAMP
-)
-USING DELTA
-COMMENT "Parameters to track air quality."
-LOCATION '{PARAMS_SILVER_LOC}'
 """)
 
 # COMMAND ----------
@@ -202,7 +182,7 @@ LOCATION '{PARAMS_SILVER_LOC}'
 def update_measure_points_silver(microbatch, batch_id):
     """
     Performs SCD Type 1 upsert to the table:
-    project_weather_air.air_quality.measure_points_silver
+    project_weather_air.air_quality.dim_measure_points
     """
 
     # deduplicate microbatch - measure points
@@ -217,24 +197,25 @@ def update_measure_points_silver(microbatch, batch_id):
         USING microbatch_measure_points AS s
         ON s.point_id = t.point_id
         WHEN MATCHED AND
-            t.point_id <> s.point_id OR
+            t.station_id <> s.station_id OR
             t.param_id <> s.param_id OR
-            t.station_id <> s.station_id
+            t.param_code <> s.param_code
         THEN UPDATE SET
-            t.point_id = s.point_id,
-            t.param_id =  s.param_id,
             t.station_id = s.station_id,
-            t.proccessed_timestamp = current_timestamp()
+            t.param_code = s.param_code,
+            t.processed_timestamp = current_timestamp()
         WHEN NOT MATCHED THEN INSERT (
             t.point_id,
-            t.param_id,
             t.station_id,
-            t.proccessed_timestamp
+            t.param_id,
+            t.param_code,
+            t.processed_timestamp
           )
         VALUES (
             s.point_id,
-            s.param_id,
             s.station_id,
+            s.param_id,
+            s.param_code,
             current_timestamp()
         )
     """
@@ -243,66 +224,14 @@ def update_measure_points_silver(microbatch, batch_id):
 
 # COMMAND ----------
 
-def update_params_silver(microbatch, batch_id):
-    """
-    Performs SCD Type 1 upsert to the tables:
-     - project_weather_air.air_quality.params_silver
-    """
-
-    # deduplicate params
-    params_deduped = microbatch.dropDuplicates(["param_id"])
-
-    # create temp view from microbatch df params
-    params_deduped.createOrReplaceTempView("microbatch_params")
-
-    # upsert query
-    upsert_query_params = """
-        MERGE INTO project_weather_air.air_quality.params_silver AS t
-        USING microbatch_params AS s
-        ON s.param_id = t.param_id
-        WHEN MATCHED AND
-            t.param_code <> s.param_code OR
-            t.param_name <> s.param_name
-        THEN UPDATE SET
-            t.param_code = s.param_code,
-            t.param_name = s.param_name,
-            t.processed_timestamp = current_timestamp()
-        WHEN NOT MATCHED THEN INSERT(
-             t.param_id,
-             t.param_code,
-             t.param_name,
-             t.processed_timestamp
-            )
-        VALUES (
-            s.param_id,
-            s.param_code,
-            s.param_name,
-            current_timestamp()
-        )
-    """
-
-    params_deduped._jdf.sparkSession().sql(upsert_query_params)
-
-# COMMAND ----------
-
 monitor_stations_to_merge_df = monitor_stations_expand_rename_cols
+
 def stream_measures_upsert_to_silver():
     writer = (monitor_stations_to_merge_df
         .writeStream
         .foreachBatch(update_measure_points_silver)
+        .outputMode("update")
         .option("checkpointLocation", f"{os.path.join(MEASURE_POINTS_SILVER_LOC, '_checkpoint')}")
-        .trigger(availableNow=True)
-        .start()
-    )
-    return writer
-
-# COMMAND ----------
-
-def stream_params_upsert_to_silver():
-    writer = (monitor_stations_to_merge_df
-        .writeStream
-        .foreachBatch(update_params_silver)
-        .option("checkpointLocation", f"{os.path.join(PARAMS_SILVER_LOC, '_checkpoint')}")
         .trigger(availableNow=True)
         .start()
     )
@@ -314,4 +243,10 @@ stream_measures_upsert_to_silver()
 
 # COMMAND ----------
 
-stream_params_upsert_to_silver()
+# %sql
+# SELECT
+#   point_id,
+#   COUNT(*)
+# FROM project_weather_air.air_quality.measure_points_silver
+# GROUP BY point_id
+# HAVING COUNT(*) > 1;
