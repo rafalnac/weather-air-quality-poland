3c2b0aff7f81137e742e7580ce81de3ceb3fc15c
Rafal Naczelnik <rafal.naczelnik@gmail.com>
2023-10-14 15:12:45 +0200

    Added general funcitons.

---
+++ etldatabricks/functions/general.py
@@ -0,0 +1,28 @@
+"""
+Module contains function used for ingestion and transformation.
+
+"""
+
+import tomli as tomlib
+
+
+def load_config_files(path: str) -> dict[str, any]:
+    """Load configuration file in TOML format.
+
+    Args:
+        path: Absolute path to file.
+
+    Returns:
+        Dictionary.
+
+    Raises:
+        TypeError: If path is not str type.
+    """
+
+    if not isinstance(path, str):
+        TypeError("Path must be a string type.")
+
+    with open(path, "rb") as cfg:
+        cfg_file = tomlib.load(cfg)
+
+    return cfg_file
